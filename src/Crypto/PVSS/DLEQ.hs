666c1fb7717a712c5cd38a470c97ed706632f57f
Vincent Hanquez <vincent.hanquez@iohk.io>
2017-03-31 06:24:48 +0100

    Add Parallel proof computing

--- src/Crypto/PVSS/DLEQ.hs
+++ src/Crypto/PVSS/DLEQ.hs
@@ -3,8 +3,13 @@
 module Crypto.PVSS.DLEQ
     ( DLEQ(..)
     , Proof(..)
+    , ParallelProof(..)
+    , ParallelProofs(..)
+    , Challenge
     , generate
     , verify
+    , generateParallel
+    , verifyParallel
     ) where
 
 import           Control.DeepSeq
@@ -39,6 +44,20 @@ data Proof = Proof
 instance Binary Proof
 instance NFData Proof
 
+newtype ParallelProof = ParallelProof { parallelProof_z :: Scalar }
+    deriving (Show,Eq,Generic)
+
+instance Binary ParallelProof
+instance NFData ParallelProof
+
+data ParallelProofs = ParallelProofs
+    { parallelProofsChallenge :: !Challenge
+    , parallelProofsValues    :: ![ParallelProof]
+    } deriving (Show,Eq,Generic)
+
+instance Binary ParallelProofs
+instance NFData ParallelProofs
+
 -- | Generate a proof
 generate :: Scalar -- ^ random value
          -> Scalar -- ^ a
@@ -62,3 +81,37 @@ verify (DLEQ g1 h1 g2 h2) (Proof (Challenge ch) r) = ch == hashPoints [h1,h2,a1,
     c = keyFromBytes ch
     a1 = r1 .- (h1 .* c)
     a2 = r2 .- (h2 .* c)
+
+-- | Generate all the proofs with a commonly computed challenge
+generateParallel :: [(Scalar, Scalar, DLEQ)]     -- (random value w, secret a, DLEQ parameter)
+                 -> ParallelProofs
+generateParallel l =
+    let as = map (\(r, _, (DLEQ g1 _ g2 _)) -> (g1 .* r, g2 .* r)) l
+        h  =  concatMap (\(_, _, (DLEQ _ v1 _ v2)) -> [v1,v2]) l
+           ++ concatMap (\(a1, a2) -> [a1,a2]) as
+        -- all v1,a2 followed by all (a1,a2)
+        challenge = hashPoints h
+        c = keyFromBytes challenge
+
+     in ParallelProofs
+            { parallelProofsChallenge = Challenge challenge
+            , parallelProofsValues    = map (\(w, a, _) -> ParallelProof (w #+ (a #* c))) l
+            }
+
+-- | verify proof with a commonly computed challenge
+verifyParallel :: [DLEQ]
+               -> ParallelProofs
+               -> Bool
+verifyParallel dleqs proofs =
+    let computed = zipWith verifyOne dleqs (parallelProofsValues proofs)
+     in ch == hashPoints (concatMap fst computed ++ concatMap snd computed)
+  where
+    (Challenge ch) = parallelProofsChallenge proofs
+    verifyOne :: DLEQ -> ParallelProof -> ([Point], [Point])
+    verifyOne (DLEQ g1 h1 g2 h2) (ParallelProof r) = ([h1,h2], [a1,a2])
+      where
+        r1 = g1 .* r
+        r2 = g2 .* r
+        c = keyFromBytes ch
+        a1 = r1 .- (h1 .* c)
+        a2 = r2 .- (h2 .* c)
