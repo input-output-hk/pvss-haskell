1ee1eee5ac70565211854e060bdce386e10139e1
Vincent Hanquez <vincent.hanquez@iohk.io>
2017-07-10 11:48:08 +0100

    add tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -3,6 +3,7 @@ module Main where
 import           Control.Monad
 import           Crypto.Random
 import qualified Crypto.PVSS as PVSS
+import qualified Crypto.SCRAPE as SCRAPE
 import           Test.Tasty
 import           Test.Tasty.QuickCheck
 
@@ -12,10 +13,21 @@ newtype Threshold = Threshold PVSS.Threshold
 newtype Participants = Participants Integer
     deriving (Show,Eq)
 
+data KofN = KofN PVSS.Threshold Integer
+    deriving (Show,Eq)
+
 instance Arbitrary Threshold where
     arbitrary = Threshold <$> choose (2,5)
 instance Arbitrary Participants where
     arbitrary = Participants <$> choose (3,10)
+
+instance Arbitrary KofN where
+    arbitrary = do
+        n <- choose (3,10)
+        t <- choose (2,5)
+        pure $ if t+2 >= n then KofN t (t+2)
+                           else KofN t n
+
 instance Show ChaChaDRG where
     show _ = "chachaDRG"
 instance Arbitrary ChaChaDRG where
@@ -82,9 +94,81 @@ testRecovery (Threshold threshold) (Participants nOrig) rng =
     (decryptedShares, _) = withDRG rng3 $ do
         mapM (\(kp,eshare) -> PVSS.shareDecrypt kp eshare) (zip participants eshares)
 
+-----------------------------------------------
+-- SCRAPE test
+
+scrapeEncryptVerify :: KofN -> ChaChaDRG -> Property
+scrapeEncryptVerify (KofN threshold nOrig) rng =
+    let (r, _) = withDRG rng3 $ SCRAPE.verifyEncryptedShares egen threshold commitments proofs eshares participants
+     in r === True
+  where
+    n :: Integer
+    n = max (threshold) nOrig
+
+    (participantAll, rng2) = withDRG rng $ replicateM (fromIntegral n) PVSS.keyPairGenerate
+    participants = SCRAPE.Participants $ map toPk participantAll
+
+    ((egen, sec, eshares, commitments, proof, proofs), rng3) = withDRG rng2 $
+        SCRAPE.escrow threshold participants
+
+scrapeDecryptVerify :: KofN -> ChaChaDRG -> Property
+scrapeDecryptVerify (KofN threshold nOrig) rng =
+        map (SCRAPE.verifyDecryptedShare) (zip3 eshares (map toPk participantAll) decryptedShares)
+    === map (const True) eshares
+  where
+    n :: Integer
+    n = max (threshold) nOrig
+
+    (participantAll, rng2) = withDRG rng $ replicateM (fromIntegral n) SCRAPE.keyPairGenerate
+    participants = SCRAPE.Participants $ map toPk participantAll
+
+    ((egen, sec, eshares, commitments, proof, proofs), rng3) = withDRG rng2 $
+        SCRAPE.escrow threshold participants
+    (decryptedShares, _) = withDRG rng3 $ do
+        mapM (\(kp,eshare) -> SCRAPE.shareDecrypt kp eshare) (zip participantAll eshares)
+
+{-
+scrapeSecretVerify :: Threshold -> Participants -> ChaChaDRG -> Property
+scrapeSecretVerify (Threshold threshold) (Participants nOrig) rng =
+    PVSS.verifySecret egen commitments sec secProof === True
+  where
+    n :: Integer
+    n = max (threshold) nOrig
+
+    (participants, rng2) = withDRG rng $ replicateM (fromIntegral n) PVSS.keyPairGenerate
+
+    ((egen, sec, secProof, commitments, _), rng3) = withDRG rng2 $
+        SCRAPE.escrow threshold (map toPk participants)
+        -}
+
+scrapeRecovery :: KofN -> ChaChaDRG -> Property
+scrapeRecovery (KofN threshold nOrig) rng =
+
+    let recovered = SCRAPE.recover $ take (fromIntegral (threshold+1)) $ zip [1..] decryptedShares
+     in recovered === sec
+
+  where
+    n :: Integer
+    n = max (threshold) nOrig
+
+    (participants, rng2) = withDRG rng $ replicateM (fromIntegral n) SCRAPE.keyPairGenerate
+
+    ((egen, sec, eshares, commitments, proof, proofs), rng3) = withDRG rng2 $
+        SCRAPE.escrow threshold (SCRAPE.Participants $ map toPk participants)
+
+    (decryptedShares, _) = withDRG rng3 $ do
+        mapM (\(kp,eshare) -> SCRAPE.shareDecrypt kp eshare) (zip participants eshares)
+
 main :: IO ()
 main = defaultMain $ testGroup "PVSS"
-    [ testProperty "encrypted-verified" testEncryptVerify
-    , testProperty "decrypted-verified" testDecryptVerify
-    , testProperty "secret-verified" testSecretVerify
-    , testProperty "recovery" testRecovery ]
+    [ testGroup "schoenmaker"
+        [ testProperty "encrypted-verified" testEncryptVerify
+        , testProperty "decrypted-verified" testDecryptVerify
+        , testProperty "secret-verified" testSecretVerify
+        , testProperty "recovery" testRecovery ]
+    , testGroup "scrape"
+        [ testProperty "encrypted-verified" scrapeEncryptVerify
+        , testProperty "decrypted-verified" scrapeDecryptVerify
+        {-, testProperty "secret-verified" scrapeSecretVerify -}
+        , testProperty "recovery" scrapeRecovery ]
+    ]
